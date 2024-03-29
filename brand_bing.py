644744ab0974cef5e7aad2ca95ecdf1af74a8bde
lisa <1004856404@qq.com>
2024-03-29 14:16:23 +0800

    Initial commit

--- brand_bing.py
+++ brand_bing.py
@@ -8,12 +8,12 @@ from get_security_code import bind_security_code
 import os
 
 def main():
-    # folder_path = 'data_test'  # 文件夹路径
-    folder_path = 'data_beta'  # 文件夹路径
+    folder_path = 'data_test'  # 文件夹路径
+    # folder_path = 'data_beta'  # 文件夹路径
     #品牌站枚举
     brands = ['elfbar','lostmary','ELFLIQ','FunkyRepublic','EBDesign','EBCREATE','FUNKY_LANDS','HIDESEEK','RabBeatsVape','LOSTY_LOSTY','MAD_EYES','OFF_STAMP','URBAN_TALE','LOSGAL','MARYLIQ']
 
-    secuirty_code_out_files = os.path.join(folder_path,brands[0]+'_secuirty_code_out.txt')  # 输出文件路径
+    secuirty_code_out_files = os.path.join(folder_path,brands[12]+'_secuirty_code_out.txt')  # 输出文件路径
 
     #调用绑定防伪码方法之前清空OUT TXT
     with open(secuirty_code_out_files,'w'):
@@ -24,7 +24,7 @@ def main():
     num = len(HS_security_id)
     print(num)
     #调用绑定防伪码方法
-    bind_security_code(folder_path,brands[0],HS_security_id,num)
+    bind_security_code(folder_path,brands[12],HS_security_id,num)
 
 if __name__ == '__main__':
     main()
