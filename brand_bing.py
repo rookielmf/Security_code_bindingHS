86d888d2b4579961eaead87cbcc291367b2274e1
rookielmf <liminfeng@liminfeng@.com>
2024-03-29 16:13:43 +0800

    '添加可以输入的序号'

--- brand_bing.py
+++ brand_bing.py
@@ -1,30 +1,47 @@
-'''
-@create : lisa
-@file :security_code
-@Date :2024/3/6
-@desc :
-'''
 from get_security_code import bind_security_code
 import os
 
 def main():
-    # folder_path = 'data_test'  # 文件夹路径
-    folder_path = 'data_beta'  # 文件夹路径
-    #品牌站枚举
-    brands = ['elfbar','lostmary','ELFLIQ','FunkyRepublic','EBDesign','EBCREATE','FUNKY_LANDS','HIDESEEK','RabBeatsVape','LOSTY_LOSTY','MAD_EYES','OFF_STAMP','URBAN_TALE','LOSGAL','MARYLIQ']
+    folder_path = input("请选择防伪码的测试环境 (输入 1 为 test 环境, 输入 2 为 beta 环境): ")
+    if folder_path == "1":
+        folder_path = "data_test"
+    elif folder_path == "2":
+        folder_path = "data_beta"
+    else:
+        print("无效输入，退出...")
+        return
 
-    secuirty_code_out_files = os.path.join(folder_path,brands[0]+'_secuirty_code_out.txt')  # 输出文件路径
+    brands = [
+        'elfbar', 'lostmary', 'ELFLIQ', 'FunkyRepublic', 'EBDesign',
+        'EBCREATE', 'FUNKY_LANDS', 'HIDESEEK', 'RabBeatsVape', 'LOSTY_LOSTY',
+        'MAD_EYES', 'OFF_STAMP', 'URBAN_TALE', 'LOSGAL', 'MARYLIQ'
+    ]
 
-    #调用绑定防伪码方法之前清空OUT TXT
-    with open(secuirty_code_out_files,'w'):
+    print("品牌编号（1-15）:")
+    for i, brand in enumerate(brands, 1):
+        print(f"{i}. {brand}", end='\t')
+        if i % 5 == 0:  # 每打印5个品牌换行
+            print()
+
+    brand_selection = input("请选择需要创建防伪码的品牌编号: ")
+    if not brand_selection.isdigit() or int(brand_selection) < 1 or int(brand_selection) > 15:
+        print("无效输入，退出...")
+        return
+
+    brand_index = int(brand_selection) - 1
+    selected_brand = brands[brand_index]
+
+    secuirty_code_out_files = os.path.join(folder_path, selected_brand + '_secuirty_code_out.txt')
+
+    with open(secuirty_code_out_files, 'w'):
         pass
-    #HS数据库防伪码ID枚举
-    HS_security_id =[17641,17642,17643,17644,17645]
-    #HS数据库防伪码ID数量
+
+    HS_security_id = [17641, 17642, 17643, 17644, 17645]
     num = len(HS_security_id)
     print(num)
-    #调用绑定防伪码方法
-    bind_security_code(folder_path,brands[0],HS_security_id,num)
+
+    bind_security_code(folder_path, selected_brand, HS_security_id, num)
+
 
 if __name__ == '__main__':
     main()
